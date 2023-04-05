fec7a68fde77b3922a5b1bc9dc1b477722610e51
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-04-05 15:28:47 +0600

    add some btn and some styles

--- src/Components/FriendDetail/FriendDetail.jsx
+++ src/Components/FriendDetail/FriendDetail.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const FriendDetail = () => {
     const friend = useLoaderData();
--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Post.css';
 
 const Post = ({ post }) => {
     const { id, title, body } = post;
+    const navigate = useNavigate();
     const handleNavigation = () => {
-        
+        navigate(`/post/${id}`);
     }
     return (
         <div className='post'>
