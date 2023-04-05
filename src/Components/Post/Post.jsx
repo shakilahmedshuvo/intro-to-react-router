c99712e04597496788d7d56c802c8694d620b6ac
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-04-05 14:32:17 +0600

    add some chnages

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Post.css'
+import './Post.css';
 
 const Post = ({ post }) => {
     const { id, title, body } = post;
+    const handleNavigation = () => {
+        
+    }
     return (
-        <div>
+        <div className='post'>
             <h5>ID: {id}</h5>
             <h5>{title}</h5>
             <Link to={`/post/${id}`}>Show Details</Link>
-            <button>Show Post Details</button>
+            <Link to={`/post/${id}`}><button>Show Post Details</button></Link>
+            <button onClick={handleNavigation}>With Button Handler</button>
         </div>
     );
 };
